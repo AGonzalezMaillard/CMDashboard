92ca1f30c60664012df659fff6e6708f1f977f67
AGonzalezMaillard <mariaaraceli.gonzalez@gracia.lasalle.cat>
2016-02-08 15:21:23 +0100

    Update forms.php

--- pages/forms.php
+++ pages/forms.php
@@ -1,16 +1,10 @@
- <?php
-include './pages/cabecera.html';
-?>
-
-<div id="page-wrapper">
+ 	<div id="page-wrapper">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1 class="page-header">Tabla de casos</h1>
+                    <h1 class="page-header">Crear caso</h1>
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
-
-
             <!-- /.row -->
             <div class="row">
                 <div class="col-lg-12">
